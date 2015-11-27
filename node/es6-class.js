2e815457d053f4f05b26f7ed84228fb1a04d002b
Shiva Chandra Kumar K <ksck23@gmail.com>
2015-11-27 17:42:54 +0530

    added es6 supported class example

---
+++ node/es6-class.js
@@ -0,0 +1,21 @@
+'use strict'
+
+class Animal { 
+  constructor(name) {
+    this.name = name;
+  }
+  
+  speak() {
+    console.log(this.name + ' makes a noise.');
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log(this.name + ' barks.');
+  }
+}
+
+var brownie = new Dog("brownie");
+
+brownie.speak();
